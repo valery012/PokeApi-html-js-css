f44d874e4f8e81ffee8fd025345a644dff0ef9da
Val <valeriabarrios355@gmail.com>
2024-09-13 08:00:29 -0500

    Cambios clase PokeApi

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Dec */
+
+const galeria =document.getElementById('galeria')
+const modal = document.getElementById('pokemonModal')
+const modalContent = document.getElementById('pokemon-detail')
+const closeModal = document.querySelector('.close')
+const serachInput = document.getElementById('search-input')
+const serachButton = document.getElementById('search-botton')
+
+//ASYNC - AWAIT 
