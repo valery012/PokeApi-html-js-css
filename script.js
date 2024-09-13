5a603c8cc6677b51b64b3ca163e90fc071b97a69
Val <valeriabarrios355@gmail.com>
2024-09-13 08:31:12 -0500

    Cambios json

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ const serachInput = document.getElementById('search-input')
 const serachButton = document.getElementById('search-botton')
 
 //ASYNC - AWAIT 
+
+async function fetchPokemons(){
+    for(let id = 1; id<= 30;id++){
+        await fetchPokemonsByIdOrName(id);
+
+    }
+}
+
+async function fetchPokemonsByIdOrName(identifier){
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`)
+        if (!response.ok){
+            alert('pokemon no encontrado');
+            return;
+                }
+                const data= await response.json();
+                createPokemonCard(data);
+    }catch(error){
+        console.error('Error obteniendo pokemon', error)
+
+    }
+}
+
+
+function createPokemonCard (pokemon){
+    const imageUrl1 = pokemon.sprites.other.home.front_default;
+    const pokemonName = pokemon.name.charAt(0).toUpperCase()+ pokemon.name.slice(1);
+    console.log(pokemonName)
+
+}
+
+fetchPokemons();
